f83ef98f5e1b217ffc3e7138bfd37f0e81fe6b99
Alex <winchell.22@osu.edu>
2017-04-13 04:37:25 -0400

    PassengerId is now mother

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ def raw_test_set():
 
 def preprocessed_training_set():
     df = pd.read_csv('data/train.csv')
-    df = df.drop(['PassengerId', 'Ticket'], 1)
-    df['Embarked'] = df['Embarked'].fillna('C')
-    #df['Fare'].fillna(df['Fare'].median(), inplace=True)
-    df['Fare'].fillna(8.05, inplace=True)
+    df = df.drop(['Ticket'], 1)
+    df['PassengerId'].loc[df['PassengerId'] > 0] = "not mother"
+    df['Embarked'] = df['Embarked'].fillna('S')
+    df['Fare'].fillna(df['Fare'].median(), inplace=True)
+    #df['Fare'].fillna(8.05, inplace=True)
     df['Cabin'].fillna('U', inplace=True)
    
     df['Cabin'] = df['Cabin'].apply(lambda x: str(x)[0])
-    #df['Age'].fillna('-1', inplace=True)
+    df['Age'].fillna(-1, inplace=True)
     df = df.fillna(0)
     
     # Replace names with titles
@@ -26,6 +27,7 @@ def preprocessed_training_set():
 
     title_map = { 'Mr':1, 'Master':2, 'Mrs':3, 'Miss':4, 'Rare':5 }
     
+    df['PassengerId'].loc[(df['Age'] > 16) & ((df['Sex'] == "female") & ((df['Parch'] > 0) & (df['Name'] == "Mrs")))] = "mother"
     # Build family size feature and make discrete
     df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
 
@@ -34,32 +36,51 @@ def preprocessed_training_set():
     df['FamilySize'].loc[df['FamilySize'] > 4] = 5 
     df['FamilySize'].loc[(df['FamilySize'] < 5) & (df['FamilySize'] > 1)] = 3 
 
-    #df['Age'].loc[(df['Age'] < 0) & ((df['Name'] == 'Miss') & (df['Parch'] > 0))] = 1
-    #df['Age'].loc[(df['Age'] < 0) & ((df['Name'] == 'Master') & (df['Parch'] > 0))] = 1
+    df['Sex'].loc[(df['Age'] < 15) & (df['Age'] > -1)] = "child"
+
+    #df['Age'].loc[(df['Age'] < 0) & ((df['Name'] == 'Miss') & (df['Parch'] > 0))] = 4
+    #df['Age'].loc[(df['Age'] < 0) & (df['Name'] == 'Master')] = 4
     #df['Age'].loc[df['Age'] < 0] = 35
-    df['Age'].loc[df['Age'] < 15] = "2"
+    df['Age'].loc[(df['Age'] < 15) & (df['Age'] > -1)] = "2"
     df['Age'].loc[df['Age'] > 64] = "1"
     df['Age'].loc[(df['Age'] < 65) & (df['Age'] > 14)] = "3"
+    df['Age'].loc[df['Age'] < 0] = "-1"
+    '''
+    sMedian = df['Fare'].loc[df['Embarked'] == "S"].median()
+    qMedian = df['Fare'].loc[df['Embarked'] == "Q"].median()
+    cMedian = df['Fare'].loc[df['Embarked'] == "C"].median()
 
-    gender_map = { "male": 1, "female": 2 } 
+    df['Fare'].loc[(df['Embarked'] == "S") & (df['Fare'] <= sMedian)] = "low"
+    df['Fare'].loc[(df['Embarked'] == "S") & (df['Fare'] != "low")] = "high"
+    df['Fare'].loc[(df['Embarked'] == "C") & (df['Fare'] <= cMedian)] = "low"
+    df['Fare'].loc[(df['Embarked'] == "C") & (df['Fare'] != "low")] = "high"
+    df['Fare'].loc[(df['Embarked'] == "Q") & (df['Fare'] <= qMedian)] = "low"
+    df['Fare'].loc[(df['Embarked'] == "Q") & (df['Fare'] != "low")] = "high"
+    '''
+    gender_map = { "male": 1, "female": 2, "child": 3 } 
     port_map = { "S": 1, "C": 2, "Q": 3 }
     cabin_map = {"U": 0, "A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "T": 8}
-    age_map = {"1": 1, "2": 2, "3": 3}
+    age_map = {"-1": 0, "1": 1, "2": 2, "3": 3}
+    #fare_map = {"low": 0, "high": 1}
+    mother_map = {"not mother": 0, "mother": 1}
 
-    df = df.replace({"Sex": gender_map, "Embarked": port_map, "Name": title_map, "Cabin": cabin_map, "Age": age_map })
+    df = df.replace({"Sex": gender_map, "Embarked": port_map, "Name": title_map, "Cabin": cabin_map, "Age": age_map, "PassengerId": mother_map})
+
+    print(df)
 
     return df
 
 def preprocessed_test_set():
     df = pd.read_csv('data/test.csv')
-    df = df.drop(['PassengerId', 'Ticket'], 1)
-    df['Embarked'] = df['Embarked'].fillna('C')
-    #df['Fare'].fillna(df['Fare'].median(), inplace=True)
-    df['Fare'].fillna(8.05, inplace=True)
+    df = df.drop(['Ticket'], 1)
+    df['PassengerId'].loc[df['PassengerId'] > 0] = "not mother"
+    df['Embarked'] = df['Embarked'].fillna('S')
+    df['Fare'].fillna(df['Fare'].median(), inplace=True)
+    #df['Fare'].fillna(8.05, inplace=True)
     df['Cabin'].fillna('U', inplace=True)
 
     df['Cabin'] = df['Cabin'].apply(lambda x: str(x)[0])
-    #df['Age'].fillna('-1', inplace=True)
+    df['Age'].fillna(-1, inplace=True)
     df = df.fillna(0)
 
     # Replace names with titles
@@ -70,6 +91,7 @@ def preprocessed_test_set():
     
     title_map = { 'Mr':1, 'Mrs':2, 'Master':3, 'Miss':4, 'Rare':5 }
 
+    df['PassengerId'].loc[(df['Age'] > 16) & ((df['Sex'] == "female") & ((df['Parch'] > 0) & (df['Name'] == "Mrs")))] = "mother"
     # Build family size feature and make discrete
     df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
     
@@ -78,19 +100,35 @@ def preprocessed_test_set():
     df['FamilySize'].loc[df['FamilySize'] > 4] = 5 
     df['FamilySize'].loc[(df['FamilySize'] < 5) & (df['FamilySize'] > 1)] = 3 
    
-    #df['Age'].loc[(df['Age'] < 0) & ((df['Name'] == 'Miss') & (df['Parch'] > 0))] = 1
-    #df['Age'].loc[(df['Age'] < 0) & ((df['Name'] == 'Master') & (df['Parch'] > 0))] = 1
+    df['Sex'].loc[(df['Age'] < 15) & (df['Age'] > -1)] = "child"
+    
+    #df['Age'].loc[(df['Age'] < 0) & ((df['Name'] == 'Miss') & (df['Parch'] > 0))] = 4
+    #df['Age'].loc[(df['Age'] < 0) & (df['Name'] == 'Master')] = 4
     #df['Age'].loc[df['Age'] < 0] = 35
-    df['Age'].loc[df['Age'] < 15] = "2"
+    df['Age'].loc[(df['Age'] < 15) & (df['Age'] > -1)] = "2"
     df['Age'].loc[df['Age'] > 64] = "1"
     df['Age'].loc[(df['Age'] < 65) & (df['Age'] > 14)] = "3"
+    df['Age'].loc[df['Age'] < 0] = "-1"
+    '''
+    sMedian = df['Fare'].loc[df['Embarked'] == "S"].median()
+    qMedian = df['Fare'].loc[df['Embarked'] == "Q"].median()
+    cMedian = df['Fare'].loc[df['Embarked'] == "C"].median()
     
-    gender_map = { "male": 1, "female": 2 } 
+    df['Fare'].loc[(df['Embarked'] == "S") & (df['Fare'] <= sMedian)] = "low"
+    df['Fare'].loc[(df['Embarked'] == "S") & (df['Fare'] != "low")] = "high"
+    df['Fare'].loc[(df['Embarked'] == "C") & (df['Fare'] <= cMedian)] = "low"
+    df['Fare'].loc[(df['Embarked'] == "C") & (df['Fare'] != "low")] = "high"
+    df['Fare'].loc[(df['Embarked'] == "Q") & (df['Fare'] <= qMedian)] = "low"
+    df['Fare'].loc[(df['Embarked'] == "Q") & (df['Fare'] != "low")] = "high"
+    '''
+    gender_map = { "male": 1, "female": 2, "child": 3 } 
     port_map = { "S": 1, "C": 2, "Q": 3 }
     cabin_map = {"U": 0, "A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "T": 8}
-    age_map = {"1": 1, "2": 2, "3": 3}
+    age_map = {"-1": 0, "1": 1, "2": 2, "3": 3}
+    #fare_map = {"low": 0, "high": 1}
+    mother_map = {"not mother": 0, "mother": 1}
 
-    df = df.replace({"Sex": gender_map, "Embarked": port_map, "Name": title_map, "Cabin": cabin_map, "Age": age_map })
+    df = df.replace({"Sex": gender_map, "Embarked": port_map, "Name": title_map, "Cabin": cabin_map, "Age": age_map, "PassengerId": mother_map})
 
     return df
 
