d752c00a01d8e3b663714e18fb7ebf2ed0b8ae54
Griffin Solimni <solimini.1@osu.edu>
2017-04-15 13:56:22 -0400

    .79 accuracy

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 def raw_training_set():
     return pd.read_csv('data/train.csv')
@@ -16,48 +18,103 @@ def preprocessed_test_set():
     df = process_data_set(df)
     return df
 
+def get_person(passenger):
+    age, sex, name, parch = passenger
+    if age < 16:
+        return 'child'
+    else:
+        if sex == 'female' and parch > 0 and name == 'Mrs':
+            return 'mother'
+        else:
+            return sex
+
+def get_mother(passenger):
+    age, name, person, parch = passenger
+    if age > 16 and person == 'female' and parch > 0 and name == 'Mrs':
+        return "yes"
+    else:
+        return "no"
+
 def process_data_set(df):
-    df = df.drop(['Ticket'], 1)
-    df['PassengerId'].loc[df['PassengerId'] > 0] = "not mother"
+    df = df.drop(['PassengerId', 'Ticket', 'Cabin'], 1)
+    
     df['Embarked'] = df['Embarked'].fillna('S')
+    
     df['Fare'].fillna(df['Fare'].median(), inplace=True)
+    df['Fare'] = df['Fare'].astype(int)
+
+     
     #df['Fare'].fillna(8.05, inplace=True)
-    df['Cabin'].fillna('U', inplace=True)
+
+    #  df['Person'] = df[['Age', 'Sex', 'Name', 'Parch']].apply(get_person, axis=1)
+    #  df = df.drop(['Sex'], 1)
    
-    df['Cabin'] = df['Cabin'].apply(lambda x: str(x)[0])
-    df['Age'].fillna(-1, inplace=True)
+    #  df['Cabin'].fillna('U', inplace=True)
+    #  df['Cabin'] = df['Cabin'].apply(lambda x: str(x)[0])
+    
+    average_age = df["Age"].mean()
+    std_age = df["Age"].std()
+    count_nan_age = df["Age"].isnull().sum()
+    
+    rand = np.random.randint(average_age - std_age, average_age + std_age, size = count_nan_age)
+
+    df["Age"][np.isnan(df["Age"])] = rand
+    df['Age'] = df['Age'].astype(int)
+
     df = df.fillna(0)
     
-    # Replace names with titles
     df['Name'].replace({'(.*, )|(\\..*)':'', 'Mlle':'Miss', 'Ms':'Miss', 'Mme':'Mrs'}, regex=True, inplace=True)
-
     common_titles = { 'Mr', 'Mrs', 'Master', 'Miss' }
-    df['Name'][df['Name'].apply(lambda x: x not in common_titles)] = "Rare"
+    df['Name'].loc[df['Name'].apply(lambda x: x not in common_titles)] = "Rare"
 
-    title_map = { 'Mr':1, 'Master':2, 'Mrs':3, 'Miss':4, 'Rare':5 }
-    
-    df['PassengerId'].loc[(df['Age'] > 16) & ((df['Sex'] == "female") & ((df['Parch'] > 0) & (df['Name'] == "Mrs")))] = "mother"
+
+    df['Person'] = df[['Age', 'Sex', 'Name', 'Parch']].apply(get_person, axis=1)
+
+
+    #  df['Mother'] = df[['Age', 'Name', 'Person', 'Parch']].apply(get_mother, axis=1)
+
     # Build family size feature and make discrete
-    df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
     
+    
+    #  print df.groupby(['SibSp'])['Survived'].plot.bar()
+    #  print df.groupby(['Parch'])['Survived'].mean()
+    
+    df['Family'] = df['SibSp'] + df['Parch'] + 1
+    #  df['Family'].loc[df['Family'] > 1] = 1
+    #  df['Family'].loc[df['Family'] == 1] = 0
+
+    #  df.drop(['SibSp', 'Parch'], axis=1, inplace=True)
+
+    #  df.drop(['SibSp', 'Parch'], axis=1, inplace=True)
+
+    #  df1 = pd.DataFrame(df.groupby(['FamilySize'])['Survived'].count())
+    #  df2 = pd.DataFrame(df.groupby(['FamilySize'])['Survived'].sum())
+    #  df1['Died'] = df1['Survived'] - df2['Survived']
+    #  df1.drop(['Survived'], 1, inplace=True)
+    #  df1['Survived'] = df2['Survived']
+#  
+    #  df1.plot.bar()
+    #  plt.show()
+    #  print df.groupby(['Sex'])['Survived'].mean()
+
     #  print df.groupby(['Sex'])['Survived'].mean()
     #  print df.groupby(['Name'])['Survived'].mean()
     #  print df.groupby(['FamilySize'])['Survived'].mean()
 
     #df = df.drop(['SibSp', 'Parch'], 1)
     
-    df['FamilySize'].loc[df['FamilySize'] > 4] = 5 
-    df['FamilySize'].loc[(df['FamilySize'] < 5) & (df['FamilySize'] > 1)] = 3 
+    #  df['FamilySize'].loc[df['FamilySize'] > 4] = 5 
+    #  df['FamilySize'].loc[(df['FamilySize'] < 5) & (df['FamilySize'] > 1)] = 3 
 
-    df['Sex'].loc[(df['Age'] < 15) & (df['Age'] > -1)] = "child"
 
     #df['Age'].loc[(df['Age'] < 0) & ((df['Name'] == 'Miss') & (df['Parch'] > 0))] = 4
     #df['Age'].loc[(df['Age'] < 0) & (df['Name'] == 'Master')] = 4
     #df['Age'].loc[df['Age'] < 0] = 35
-    df['Age'].loc[(df['Age'] < 15) & (df['Age'] > -1)] = "2"
-    df['Age'].loc[df['Age'] > 64] = "1"
-    df['Age'].loc[(df['Age'] < 65) & (df['Age'] > 14)] = "3"
-    df['Age'].loc[df['Age'] < 0] = "-1"
+
+    #  df['Age'].loc[(df['Age'] < 15) & (df['Age'] > -1)] = "2"
+    #  df['Age'].loc[df['Age'] > 64] = "1"
+    #  df['Age'].loc[(df['Age'] < 65) & (df['Age'] > 14)] = "3"
+    #  df['Age'].loc[df['Age'] < 0] = "-1"
     '''
     sMedian = df['Fare'].loc[df['Embarked'] == "S"].median()
     qMedian = df['Fare'].loc[df['Embarked'] == "Q"].median()
@@ -70,14 +127,31 @@ def process_data_set(df):
     df['Fare'].loc[(df['Embarked'] == "Q") & (df['Fare'] <= qMedian)] = "low"
     df['Fare'].loc[(df['Embarked'] == "Q") & (df['Fare'] != "low")] = "high"
     '''
-    gender_map = { "male": 1, "female": 2, "child": 3 } 
-    port_map = { "S": 1, "C": 2, "Q": 3 }
-    cabin_map = {"U": 0, "A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "T": 8}
-    age_map = {"-1": 0, "1": 1, "2": 2, "3": 3}
-    #fare_map = {"low": 0, "high": 1}
-    mother_map = {"not mother": 0, "mother": 1}
 
-    df = df.replace({"Sex": gender_map, "Embarked": port_map, "Name": title_map, "Cabin": cabin_map, "Age": age_map, "PassengerId": mother_map})
+    #  median = df['Fare'].median()
+#  
+    #  df['Fare'].loc[(df['Fare'] < median / 3.0)] = 1
+    #  df['Fare'].loc[(df['Fare'] >= median / 3.0) & (df['Fare'] <= median * 2.0 / 3.0)] = 2
+    #  df['Fare'].loc[(df['Fare'] > median * 2.0 / 3.0)] = 3
+    
+    #  df.drop(['Age'], axis=1, inplace=True)
+
+    #  print df['Fare'].isnull().values.any() 
+    
+    gender_map = { "male": 1, "female": 2, "child": 3, "mother": 4} 
+    port_map = { "S": 1, "C": 2, "Q": 3 }
+    cabin_map = { "U": 0, "A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "T": 8 }
+    sex_map = {"male": 0, "female": 1 }
+    #  age_map = {"-1": 0, "1": 1, "2": 2, "3": 3}
+    title_map = { 'Mr':1, 'Master':2, 'Mrs':3, 'Miss':4, 'Rare':5 }
+    mother_map = {"no": 0, "yes": 1 }
+    #fare_map = {"low": 0, "high": 1}
+    
+
+    df.replace({ "Person": gender_map, "Embarked": port_map, "Name": title_map, "Sex": sex_map }, inplace=True)
+
+    # "Cabin": cabin_map
+    #  "Age": age_map
 
     #print(df)
 
@@ -89,8 +163,6 @@ def create_submission_csv(classifications):
     df.to_csv('data/result.csv', index=False)
 
 if __name__ == "__main__":
-    print "Train: "
     print preprocessed_training_set().head()
-    print "Test: "
     print preprocessed_test_set().head()
 
--- test.py
+++ test.py
@@ -3,6 +3,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neural_network import MLPClassifier
+
+import matplotlib.pyplot as plt
 
 import pandas as pd
 import numpy as np
@@ -11,32 +15,68 @@ import setup as s
 train_X = s.preprocessed_training_set().drop(['Survived'], axis=1)
 train_Y = s.preprocessed_training_set()['Survived']
 
-logreg = LogisticRegression()
-logreg.fit(train_X, train_Y)
-print "Logistic Regression: " + str(logreg.score(train_X, train_Y))
+#  logreg = LogisticRegression()
+#  logreg.fit(train_X, train_Y)
+#  print "Logistic Regression: " + str(logreg.score(train_X, train_Y))
+#  
+rf = RandomForestClassifier(n_estimators=1000, max_depth=3)
+rf.fit(train_X, train_Y)
+print "Random Forest: " + str(rf.score(train_X, train_Y))
+#  
 
-random_forest = RandomForestClassifier(n_estimators=100)
-random_forest.fit(train_X, train_Y)
-print "Random Forest: " + str(random_forest.score(train_X, train_Y))
+#  err_values = []
+#  for i in range(1, 140):
+    #  random_forest = RandomForestClassifier(n_estimators=i)
+    #  random_forest.fit(train_X, train_Y)
+    #  err_values.append(random_forest.score(train_X, train_Y))
+#  
+#  plt.plot(range(1, 140), err_values, '-')
+#  
+#  plt.ylabel('training error')
+#  plt.xlabel('number of estimators')
+#  plt.show()
 
-linsvm = LinearSVC()
-linsvm.fit(train_X, train_Y)
-print "Linear SVM: " + str(linsvm.score(train_X, train_Y))
+mlp = MLPClassifier()
+mlp.fit(train_X, train_Y)
+print "Neural Net: " + str(mlp.score(train_X, train_Y))
+
+#  linsvm = LinearSVC()
+#  linsvm.fit(train_X, train_Y)
+#  print "Linear SVM: " + str(linsvm.score(train_X, train_Y))
 
 knn = KNeighborsClassifier(n_neighbors = 3)
 knn.fit(train_X, train_Y)
-print "KNearest: " + str(knn.score(train_X, train_Y))
+print "KNN: " + str(knn.score(train_X, train_Y))
+#  
+#  err_values = []
+#  for i in range(1, 100):
+    #  knn = KNeighborsClassifier(n_neighbors = i)
+    #  knn.fit(train_X, train_Y)
+    #  err_values.append(knn.score(train_X, train_Y))
+#  
+#  plt.plot(range(1,100), err_values, '-')
+#  
+#  plt.ylabel('training error')
+#  plt.xlabel('number of neighbors')
+#  plt.show()
 
-nb = GaussianNB()
-nb.fit(train_X, train_Y)
-print "Naive Bayes: " + str(nb.score(train_X, train_Y))
+#  nb = GaussianNB()
+#  nb.fit(train_X, train_Y)
+#  print "Naive Bayes: " + str(nb.score(train_X, train_Y))
 
 test_X = s.preprocessed_test_set()
 
-submission = pd.DataFrame({
+knn_submission = pd.DataFrame({ 
     "PassengerId": pd.read_csv('data/test.csv')['PassengerId'],
-    "Survived": random_forest.predict(test_X)
+    "Survived": knn.predict(test_X)
 })
   
-submission.to_csv('data/result.csv', index=False)
+knn_submission.to_csv('data/knn_result.csv', index=False)
+
+rf_submission = pd.DataFrame({ 
+    "PassengerId": pd.read_csv('data/test.csv')['PassengerId'],
+    "Survived": rf.predict(test_X)
+})
+  
+rf_submission.to_csv('data/rf_result.csv', index=False)
 
